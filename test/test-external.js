411ac6ac5cafe3aab1b450be607866c018bd5177
Nathan Rajlich <nathan@tootallnate.net>
2012-08-05 14:54:05 -0700

    update test external

--- test/test-external.js
+++ test/test-external.js
@@ -3,8 +3,8 @@ var natpmp = require('../');
 
 var client = new natpmp.Client('10.0.1.1');
 
-client.externalIp(function (err, ip) {
+client.externalIp(function (err, info) {
   if (err) throw err;
-  console.log('External IP Address:', ip);
+  console.log('External IP Address:', info.ip.join('.'));
   client.close();
 });
