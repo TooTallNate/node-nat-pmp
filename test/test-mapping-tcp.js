07f6300ef5647e04eeb88045705533e1e62928df
Nathan Rajlich <nathan@tootallnate.net>
2012-08-18 15:21:38 -0700

    test: add tcp and udp mappings tests

--- test/test-mapping-tcp.js
+++ test/test-mapping-tcp.js
@@ -1,5 +1,6 @@
 
 var natpmp = require('../');
+var assert = require('assert');
 var netroute = require('netroute');
 var gateway = netroute.getGateway();
 
@@ -7,6 +8,8 @@ var client = new natpmp.Client(gateway);
 
 client.portMapping({ public: 3000, private: 3000 }, function (err, info) {
   if (err) throw err;
+  assert.equal(3000, info.private);
+  assert.equal('tcp', info.type);
   console.log('Port Mapping:', info);
   client.close();
 });
